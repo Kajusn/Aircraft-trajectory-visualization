3da323aaad7450615177738a2b9f230004ff8307
kajusn <kajusn@gmail.com>
2020-02-11 22:26:27 +0200

    Added Load data button click handler to call the datamanager class

---
+++ Assets/Scripts/OnClickHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OnClickHandler : MonoBehaviour
+{
+    void Start()
+    {
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        DataManager dm = gameObject.GetComponent<DataManager>();
+        dm.GetComponent<DataManager>().ReadFlightsFromFile();
+    }
+}
