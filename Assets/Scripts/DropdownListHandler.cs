77cf2d889263b9b2568cc87d96c86ef2d3e8698f
kajusn <kajusn@gmail.com>
2020-02-12 10:32:02 +0200

    Can successfully select any flight from the list and it shows up in the UI

--- Assets/Scripts/DropdownListHandler.cs
+++ Assets/Scripts/DropdownListHandler.cs
@@ -6,22 +6,34 @@ using System.Linq;
 
 public class DropdownListHandler : MonoBehaviour
 {
+    private List<string> options;
     private Dropdown dropdown;
+    private Text selectedItem;
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        selectedItem.text = options[index];
+    }
 
     public void Populate(Hashtable table)
     {
         dropdown = gameObject.GetComponent<Dropdown>();
         // Get all available flights
-        List<string> L = table.Keys.Cast<string>().ToList();
+        options = table.Keys.Cast<string>().ToList();
         dropdown.ClearOptions();
-
-        var i = 0;
+        dropdown.GetComponent<Dropdown>().AddOptions(options);
+        /*var i = 0;
         foreach (string option in L)
         {
             dropdown.options.Add(new Dropdown.OptionData(option));
             dropdown.options[i].text = option;
             i++;
-        }
-        dropdown.GetComponent<Dropdown>().AddOptions(L);
+        }*/
+    }
+
+    void Start()
+    {
+        var text = GameObject.Find("Flight_Text");
+        this.selectedItem = text.GetComponent<Text>();
     }
 }
