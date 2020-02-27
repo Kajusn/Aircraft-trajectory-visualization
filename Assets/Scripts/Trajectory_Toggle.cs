a3665bef809da7c344a87832557b095b9730d155
Kajus Naujokaitis <kajusn@gmail.com>
2020-02-27 15:01:15 +0200

    Added trajectory On/Off toggle

---
+++ Assets/Scripts/Trajectory_Toggle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Trajectory_Toggle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject trajectoryRenderer;
+
+    public void ToggleBehavior(bool toggle)
+    {
+        trajectoryRenderer.SetActive(toggle);
+    }
+}
