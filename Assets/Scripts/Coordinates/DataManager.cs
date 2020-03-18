04d1cb727d0fc4d9d9001d8c26ea4594f8a20551
Kajus Naujokaitis <kajusn@gmail.com>
2020-03-18 17:50:28 +0200

    Added landing procedure coordinates, datamanager reads them from file

--- Assets/Scripts/Coordinates/DataManager.cs
+++ Assets/Scripts/Coordinates/DataManager.cs
@@ -9,9 +9,18 @@ public class DataManager : MonoBehaviour
 {
     private string defaultPath = @"C:\Users\kajus\Aircraft-trajectory-visualization\Assets\flights.csv";
     private string altPath = @"C:\Users\kajus\Aircraft trajectory visualization\Assets\flights.csv";
+    private string approach1 = @"C:\Users\kajus\Aircraft-trajectory-visualization\Assets\d8_pom.csv";
+    private string approach1Alt = @"C:\Users\kajus\Aircraft trajectory visualization\Assets\d8_pom.csv";
+    private string approach2 = @"C:\Users\kajus\Aircraft-trajectory-visualization\Assets\d8_pom_final_approach.csv";
+    private string approach2Alt = @"C:\Users\kajus\Aircraft trajectory visualization\Assets\d8_pom_final_approach.csv";
+    private string approach3 = @"C:\Users\kajus\Aircraft-trajectory-visualization\Assets\final_approach.csv";
+    private string approach3Alt = @"C:\Users\kajus\Aircraft trajectory visualization\Assets\final_approach.csv";
 
     public Hashtable coordinatesList { get; private set; }
     public List<string> keys { get; private set; }
+
+    public Hashtable approach { get; private set; }
+    public List<string> approachKeys { get; private set; }
 
     private int NM_M = 1852;            // Nautical miles to meters conversion
     private double FT_M = 0.3048;       // Feet to meters conversion
@@ -20,13 +29,19 @@ public class DataManager : MonoBehaviour
     // Checks if data file exists
     void Awake()
     {
+        var approachFiles = new List<string>();
+        approachFiles.Add(approach1); approachFiles.Add(approach2); approachFiles.Add(approach3);
+        var approachFilesAlt = new List<string>();
+        approachFiles.Add(approach1Alt); approachFiles.Add(approach2Alt); approachFiles.Add(approach3Alt);
         if (File.Exists(defaultPath))
         {
             coordinatesList = ReadFlightsFromFile(defaultPath);
+            approach = ApproachFromFile(approachFiles);
         }
         else if (File.Exists(altPath))
         {
             coordinatesList = ReadFlightsFromFile(altPath);
+            approach = ApproachFromFile(approachFilesAlt);
         }
         else
             Debug.LogError("Could not initialize Data Manager component - File does not exist");
@@ -65,6 +80,42 @@ public class DataManager : MonoBehaviour
             return flightsTable;
         }
     }
+
+    // Reads approach coordinates from CSV files
+    private Hashtable ApproachFromFile(List<string> FilePaths)
+    {
+        var coordinatesList = new List<Coordinates>();
+        var approachTable = new Hashtable();
+
+        foreach(var file in FilePaths)
+        {
+            using (var reader = new StreamReader(file))
+            {
+                var line = reader.ReadLine();   // First line of data is unnecessary
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    var data = line.Split(',');
+                    var coordinates = new Coordinates(
+                    Convert.ToDouble(data[2]) * NM_M * UnitRatio + -34.06161, 
+                    Convert.ToDouble(data[3]) * NM_M * UnitRatio + -28.2831,
+                    Convert.ToDouble(data[4]) * FT_M * UnitRatio + 0.16152,
+                    data[1]);
+
+                    if (!approachTable.ContainsKey(data[1]))
+                    {
+                        var newList = new List<Coordinates>();
+                        newList.Add(coordinates);
+                        approachTable.Add(data[1], newList);
+                    }
+                    else
+                        ((List<Coordinates>)approachTable[data[1]]).Add(coordinates);
+                }
+            }
+        }
+        keys = approachTable.Keys.Cast<string>().ToList();
+        return approachTable;
+    }
 }
 
 public class Coordinates
@@ -82,5 +133,11 @@ public class Coordinates
         this.z = z;
         this.flight = flight;
         this.time = time;
+    }
+    public Coordinates(double x, double y, double z, string flight)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 }
