e1d64e2ca55a244b7ce5b4249ca2b17a25c0a473
Kajus Naujokaitis <kajusn@gmail.com>
2020-03-20 14:28:44 +0200

    Added simulation manager object and camera manager as its component

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera idleCamera;
+    [SerializeField]
+    private Camera aircraftCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        idleCamera.enabled = true;
+        aircraftCamera.enabled = false;
+    }
+
+    // Switch to aircraft camera
+    public void SwitchAircraftCamera()
+    {
+        if (aircraftCamera.enabled == false)
+        {
+            aircraftCamera.enabled = true;
+            idleCamera.enabled = false;
+        }
+    }
+    // Switch to idle camera
+    public void SwitchIdleCamera()
+    {
+        if (idleCamera.enabled == false)
+        {
+            idleCamera.enabled = true;
+            aircraftCamera.enabled = false;
+        }
+    }
+}
